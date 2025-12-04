99269c4556c754a4e6b25e546198dcaac042ab5a
Nizami Mursudlu <51750425+nizamimursudlu@users.noreply.github.com>
2025-12-04 14:45:03 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "eu-west-1"
-  access_key = "AKIATO6YHJOBV3WA5AV7"
-  secret_key = "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"
-}
-
 resource "aws_instance" "web" {
     ami           = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
